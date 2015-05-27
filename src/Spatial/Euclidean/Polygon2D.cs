873637df0a5a0dfdaba55ff4c164f20349acd9a0
Matt Jarvis <mattj23@gmail.com>
2015-05-27 00:00:39 -0400

    Work on convex hull algorithm

--- src/Spatial/Euclidean/Polygon2D.cs
+++ src/Spatial/Euclidean/Polygon2D.cs
@@ -50,6 +50,84 @@ namespace MathNet.Spatial.Euclidean
             return c;
         }
 
+        public static Polygon2D GetConvexHullFromPoints(IEnumerable<Point2D> pointList)
+        {
+            // Use the Quickhull algorithm to compute the convex hull of the given points, 
+            // making the assumption that the points were delivered in no particular order.
+            var points = new List<Point2D>(pointList);
+
+            // Perform basic validation of the input point cloud for cases of less than
+            // four points being given
+            if (points.Count <= 1)
+                return null;
+            if (points.Count <= 3)
+                return new Polygon2D(points);
+
+            // Find the leftmost and rightmost points
+            Point2D leftMost = points.First();
+            Point2D rightMost = points.First();
+            foreach (var point in points)
+            {
+                if (point.X < leftMost.X)
+                    leftMost = point;
+                if (point.X > rightMost.X)
+                    rightMost = point;
+            }
+
+            // Remove the left and right points
+            points.Remove(leftMost);
+            points.Remove(rightMost);
+
+            // Break the remaining cloud into upper and lower sets
+            var upperPoints = new List<Point2D>();
+            var lowerPoints = new List<Point2D>();
+            Vector2D chord = leftMost.VectorTo(rightMost);
+            foreach (var point2D in points)
+            {
+                Vector2D testVector = leftMost.VectorTo(point2D);
+                if (chord.CrossProduct(testVector) > 0)
+                    upperPoints.Add(point2D);
+                else
+                    lowerPoints.Add(point2D);
+            }
+
+            var hullPoints = new List<Point2D>{leftMost, rightMost};
+
+            while (upperPoints.Any())
+            {
+                // Locate the furthest point 
+            }
+
+
+        }
+
+        /// <summary>
+        /// Recursive method to isolate the points from the working list which lie on the convex hull
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="workingList"></param>
+        /// <param name="hullList"></param>
+        private void RecursiveHullComputation(Point2D a, Point2D b, List<Point2D> workingList, List<Point2D> hullList)
+        {
+            if (!workingList.Any())
+                return;
+            if (workingList.Count == 1)
+            {
+                hullList.Add(workingList.First());
+                workingList.Remove(workingList.First());
+                return;
+            }
+
+            // Find the furthest point from the line
+            var chord = a.VectorTo(b);
+            foreach (var point2D in workingList)
+            {
+                var testVector = a.VectorTo(point2D);
+                
+            }
+        }
+
         public IEnumerator<Point2D> GetEnumerator()
         {
             return this._points.GetEnumerator();
